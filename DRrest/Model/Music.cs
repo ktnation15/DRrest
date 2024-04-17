820b4d3d4cdc68a22bba10265a88e6ba1bd70ca2
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-04-17 11:09:39 +0200

    Add project files.

---
+++ DRrest/Model/Music.cs
@@ -0,0 +1,15 @@
+﻿namespace DRrest.Model
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}, {Artist}, {Duration}, {PublicationYear}";
+        }
+    }
+}
---
+++ DRrest/Repository/MusicRepository.cs
@@ -0,0 +1,24 @@
+﻿using DRrest.Model;
+
+namespace DRrest.Repository
+{
+    public class MusicRepository
+    {
+        List<Music> musicList;
+
+        public MusicRepository()
+        {
+            musicList = new List<Music>()
+            {
+                new Music() {Title="House of Rock", Artist="Elvis", Duration = 3, PublicationYear=1989},
+                new Music() {Title="House of Pop", Artist="Michael", Duration = 4, PublicationYear=1990},
+                new Music() {Title="House of Jazz", Artist="Awais", Duration = 5, PublicationYear=1991},
+                new Music() {Title="House of Metal", Artist="Ibo", Duration = 6, PublicationYear=1992}
+            };
+        }
+        public List<Music> GetMusicList()
+        {
+            return musicList;
+        }
+    }
+}
